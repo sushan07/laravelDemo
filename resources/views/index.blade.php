b96194dd5c17142a49121e1713f11d1a98010085
Sushan <sushankarki7@gmail.com>
2021-11-27 16:09:51 +0545

    index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,5 @@
 <h1>CARS</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur ullam exercitationem quia consequuntur, in, magnam vero consectetur similique ipsa cumque eligendi voluptate molestiae! Facere!
+
+
+</p>
